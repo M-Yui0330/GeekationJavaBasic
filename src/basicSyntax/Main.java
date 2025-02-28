4276c8683373681b2e5873fc233917e78b7c3c41
M-Yui0330 <kuwamats.yui@gmail.com>
2025-02-28 17:27:06 +0900

    Java課題の実施　クラス

--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -1,12 +1,16 @@
 package basicSyntax;
 
 public class Main {
-  public static void main(String[] args) {		
-		  System.out.println(1);
-		  System.out.println(-1);
-		  System.out.println(5 + 1);
-		
-		  System.out.println("5 + 1");
-		  System.out.println("5" + "1");
+  public static void main(String[] args) {
+	 GeekIntroduction geek = new GeekIntroduction();
+	  
+	 int age = 24;
+	 double height = 170.1;
+	 geek.greeting(age, height);
+	 
+	 int num1 = 10;
+	 int num2 = 40;
+	 geek.specialSkill(num1, num2);
+	 
 	}
 }
